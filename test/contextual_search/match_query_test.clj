304eb26f43602dcd45f75ea091dcbb054cca060d
jurko <jurko.bossetroll@gmail.com>
2023-10-03 12:52:16 +0200

    Add match-query test

---
+++ test/contextual_search/match_query_test.clj
@@ -0,0 +1,37 @@
+(ns contextual-search.match-query-test
+  (:require [clojure.test :refer [deftest is]]
+            [contextual-search.match-query :as match]))
+
+
+(def words-test-1 ["invention" "discloses" "a" "carbon" "fiber" "solar" "panel" "car" "roof"])
+
+(def words-test-2 ["invention" "discloses" "a" "carbon" "fiber"])
+
+(def words-test-3 ["solar" "invention" "discloses" "a" "carbon" "fiber" "panel" "car" "roof"])
+
+(def query-1 {:words-to-find ["solar"]
+              :conditions    []})
+
+(def query-2 {:words-to-find ["panel" "solar"]
+              :conditions    [[{:word-1 "panel" :word-2 "solar" :max-distance 1}]]})
+
+
+(deftest single-word-query?-test
+  (is (true? (match/single-word-query? query-1 words-test-1)))
+  (is (false? (match/single-word-query? query-1 words-test-2)))
+  (is (false? (match/single-word-query? query-1 []))))
+
+
+(deftest mani-words-query?-test
+  (is (true? (match/mani-words-query? query-2 words-test-1)))
+  (is (false? (match/mani-words-query? query-2 words-test-2)))
+  (is (false? (match/mani-words-query? query-2 words-test-3))))
+
+
+(deftest match-query?-test
+  (is (true? (match/match-query? query-1 words-test-1)))
+  (is (false? (match/match-query? query-1 words-test-2)))
+  (is (false? (match/match-query? query-1 [])))
+  (is (true? (match/match-query? query-2 words-test-1)))
+  (is (false? (match/match-query? query-2 words-test-2)))
+  (is (false? (match/match-query? query-2 words-test-3))))
